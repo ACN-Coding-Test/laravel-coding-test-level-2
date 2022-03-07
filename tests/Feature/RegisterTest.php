b2b678309844a0ecf1813575f235a9cffaf1b523
Reben-dalshad <reben@trinergydigital.com>
2022-03-07 22:38:29 +0800

    Write test

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->post('api/v1/users');
+
+        $response->assertStatus(200);
+    }
+}
