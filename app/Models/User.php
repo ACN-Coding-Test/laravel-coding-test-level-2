d3ed6ba405c4ced172bc23ef02821625c671e127
Adri Haiqal <edrryy@gmail.com>
2022-07-09 00:54:56 +0800

    Create models

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'user_id');
+    }
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class, 'project_user');
+    }
+
 }
