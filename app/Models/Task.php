3088349f7d9df3a5133c07db0320dd1bba325b5b
cortzuy <cortzph@yahoo.com>
2022-08-25 22:32:24 +0800

    Create models

---
+++ app/Models/Task.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+    protected $fillable = ['title', 'description', 'status', 'project_id', 'user_id'];
+    
+    public function users()
+    {
+        return $this->hasOne(User::class);
+    }
+    
+    public function projects()
+    {
+        return $this->hasOne(Project::class);
+    }
+}
