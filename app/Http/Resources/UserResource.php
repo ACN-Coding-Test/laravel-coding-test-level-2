63c9b7654e0879bc728d3419c7dca557e3564511
Ahmad Najmi <najmiemon4223@gmail.com>
2022-09-08 18:32:21 +0800

    Add postman collection

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,7 +17,7 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'username' => $this->username,
-            'password' => $this->password,
+            // 'password' => $this->password,
         ];
     }
 }
