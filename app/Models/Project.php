447e85b5e37aecc75ae7a05ca7b3e8b686bc82d7
fiq 265 <fiq265@yahoo.com>
2022-11-06 22:56:01 +0800

    Create models

---
+++ app/Models/Project.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\UUID;
+
+class Project extends Model
+{
+    use HasFactory, UUID;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
