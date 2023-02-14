4b873c73056d31a587f3934ea0fd01ec8ed40176
Harshada <harshada.palange@accenture.com>
2023-02-14 17:04:23 +0530

    Implement pagination

--- app/Http/Controllers/Api/V1/ProjectController.php
+++ app/Http/Controllers/Api/V1/ProjectController.php
@@ -10,9 +10,30 @@ use App\Http\Controllers\Controller;
 class ProjectController extends Controller
 {
         /* function to fetch all Projects */
-        public function index()
+        public function index(Request $request)
         {
-            return Project::all();
+            //echo "<pre>";print_r($request->sortDirection);die;
+            $q = $request->q; // search keyword, will search for name
+
+            $pageIndex  = $request->pageIndex  ? $request->pageIndex : 0;  //  the index of the page to shown, default 0
+
+            $pageSize  = $request->pageSize  ? $request->pageSize  : 3; //  how many items to return, default 3
+
+            $sortBy = $request->sortBy ? $request->sortBy : 'name' ; // attribute to sort, default name 
+
+            $sortDirection  = $request->sortDirection ? $request->sortDirection : 'ASC'; //  direction of the sort, default ASC
+
+            $query =Project::query();
+
+            // Search by name
+            if (isset($q)) {
+                $query->where('name', 'LIKE', '%'.$q.'%');
+            }
+
+            // search by sort by and direction
+            $query->orderBy($sortBy, $sortDirection);
+            
+            return $query->paginate($pageSize);
         }
     
         /* function to fetch Project for which id given in route */
