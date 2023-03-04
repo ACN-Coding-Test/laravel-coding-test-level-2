5d52deaa50926d1c8c595e8897d147d9616d818e
anoop jain <anoop.jain.09@gmail.com>
2023-03-04 09:56:13 +0530

    Create REST APIs

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+
+Route::group(['prefix'=>'v1'], function (){
+    Route::get("users/{id?}",[UserController::class, 'getresources']);
+    Route::post("users",[UserController::class, 'createresource']);
+    Route::put("users/{id}",[UserController::class, 'updateresource']);
+    Route::delete("users/{id}",[UserController::class, 'deleteresource']);
+
+    Route::get("projects/{id?}",[ProjectController::class, 'getprojects']);
+    Route::post("projects",[ProjectController::class, 'createproject']);
+    Route::put("projects/{id}",[ProjectController::class, 'updateproject']);
+    Route::delete("projects/{id}",[ProjectController::class, 'deleteproject']);
+
+    Route::get("tasks/{id?}",[TaskController::class, 'getTasks']);
+    Route::post("tasks",[TaskController::class, 'createtask']);
+    Route::put("tasks/{id}",[TaskController::class, 'updatetask']);
+    Route::delete("tasks/{id}",[TaskController::class, 'deletetask']);
+
 });
