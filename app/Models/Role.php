a00a12da723aeddd0ed26faf661e0431955389dc
H.L.D Madhusanka <90693435+dhanukaMadhusanka@users.noreply.github.com>
2023-03-05 17:10:07 +0530

    Create models

---
+++ app/Models/Role.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    // role types
+    public const ROLE_TYPE_ADMIN = 1;
+    public const ROLE_TYPE_PRODUCT_OWNER = 2;
+    public const ROLE_TYPE_DEVELOPER = 3;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
---
+++ database/migrations/2023_03_04_131459_create_tasks_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('description')->nullable();
+
+            $table->tinyInteger('status')
+                ->comment('1 - not_started/2 - in_progress/3 - ready_for_test/4 - completed')
+                ->index();
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('project_id');
+
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
